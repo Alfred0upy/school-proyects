824d9eebf8d1d36f3a6be0084a6607aada0972a2
Alfred0upy <46355632+Alfred0upy@users.noreply.github.com>
2020-03-04 19:12:15 -0600

    pointer

--- test4.c
+++ test4.c
@@ -1,15 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-int main(void){
 int x = 0;
-printf("%p/n", (void *)&x);
+int main(void){
+  printf("%p/n", (void *)&x);
 
-int *px, not_a_pointer;
-px = &x;
-printf("%p/n", (void *)px);
-
-printf("%zu, %zu\n", sizeof(px), sizeof(not_a_pointer));
+  int *px, not_a_pointer;
+  px = &x;
+  printf("%p/n", (void *)px);
+  printf("%zu, %zu\n", sizeof(px), sizeof(not_a_pointer));
 return 0;
 }
 
