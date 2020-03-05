5a768fa12bf12a4d85c05b4d0ebdca27eba58560
Alfred0upy <46355632+Alfred0upy@users.noreply.github.com>
2020-03-04 19:14:56 -0600

    array

--- test5.c
+++ test5.c
@@ -1,15 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 int main(void){
 int x_array[20];
 int xx;
 for (xx = 0; xx < 20; xx++) {
 	x_array[xx] = 20 - xx;
-}
-int* x_ptr = x_array;
-printf("%d\n", *(x_ptr + 5));
-printf("%d\n", x_array[2]);
+	}
+	int* x_ptr = x_array;
+	printf("%d\n", *(x_ptr + 5));
+	printf("%d\n", x_array[2]);
 return 0;
 }
 
