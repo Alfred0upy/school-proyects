639b5deea3439f8ae56d302f8e7842e184879871
Alfred0upy <46355632+Alfred0upy@users.noreply.github.com>
2020-03-04 19:21:35 -0600

    swaptwonumbers

--- test7.c
+++ test7.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
-void swapTwoNumbers(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-int first = 10;
-int second = 20;
-printf("first: %d\nsecond: %d\n", first, second);
-swatTwoNumbers(&first, &second);
-printf("first: %d\nsecond: %d\", first, second); 
-{
-	int count = 0; 
+void swapTwoNumbers(int *a, int *b){
+    	int temp = *a;
+    	*a = *b;
+    	*b = temp;
+	}
+	int first = 10;
+	int second = 20;
+		printf("first: %d\nsecond: %d\n", first, second);
+	swapTwoNumbers(&first, &second);
+		printf("first: %d\nsecond: %d\", first, second);{
+		int count = 0; 
 	while (count < 10);
 		inc_count(&count);
 		printf("count: %d, address: %p\n", count, &count);
