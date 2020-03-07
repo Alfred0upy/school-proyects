ad167a32cbffa8c108b9a46642688463f4db5456
Alfred0upy <46355632+Alfred0upy@users.noreply.github.com>
2020-03-06 19:54:29 -0600

    ArgcArgvPrimeNumber

---
+++ testprime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main (int argc, char** argv){
+int i;
+int inte1 = atoi(argv[1]);
+int inte2 = atoi(argv[2]);
+	for(i = 0; i < argc; i++){
+		printf("argv[%d] is %s \n",argv[i]);
+                        if (inte1 = 1) 
+                                printf("1\n");
+			while (inte2 >= inte1){
+			inte1 = inte1 + 1;
+			if (inte1%2 = 0 && inte1%3 = 0);
+				printf("inte1\n");
+			break;
+			}
+	}
+}
+
